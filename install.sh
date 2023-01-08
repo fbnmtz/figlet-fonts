de3367453aa26005ca427b28c45746ac40c2faf6
Fabiano Matos <fabiano.matoz@gmail.com>
2023-01-08 11:29:08 -0300

    move fonts to new dir * update instal

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@
 # Created: Sunday, 2023/01/08 - 11:17:13
 # Author.: @fbnmtz, (fabiano.matoz@gmail.com)
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Last Modified: Sunday, 2023/01/08 - 11:24:05
+# Last Modified: Sunday, 2023/01/08 - 11:28:48
 # Modified By..: @fbnmtz, (fabiano.matoz@gmail.com)
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Version: 0.0.1.6
+# Version: 0.0.1.8
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
 # Description: 
 #  >
@@ -21,4 +21,4 @@
 git clone https://github.com/fbnmtz/figlet-fonts ./fbnmtz/figlet/
 
 # save alias to use new fonts
-echo "alias figlet='figlet -d ./fbnmtz/figlet/'" >> .bashrc
+echo "alias figlet='figlet -d ./fbnmtz/figlet/fonts'" >> .bashrc
