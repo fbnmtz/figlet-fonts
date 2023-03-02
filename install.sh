fe2403b59898a36f103afdddae6e1ea042feaff2
Fabiano Matos <fabiano.matoz@gmail.com>
2023-03-01 21:48:43 -0300

    fix install

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #
 # ############################################################################
-# Project: figlet-fonts (none)
+# Project: figlet-fonts (vundefined)
 # File...: install.sh
 # Created: Sunday, 2023/01/08 - 11:17:13
 # Author.: @fbnmtz, (fabiano.matoz@gmail.com)
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Last Modified: Saturday, 2023/01/14 - 10:54:06
+# Last Modified: Wednesday, 2023/03/01 - 21:48:21
 # Modified By..: @fbnmtz, (fabiano.matoz@gmail.com)
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Version: 0.0.2.30
+# Version: 0.0.3.32
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
 # Description: 
 #  > download a collection of fonts and create an alias for 'figlet'
@@ -24,5 +24,5 @@
 # save alias to use new fonts
 if ! grep "alias figlet=" ~/.profile; then
     echo -e "\n# add alias to figlet to include directory with fonts\n\
-        alias figlet='figlet -d ~/.fbnmtz/figlet/fonts'\n" >> ~/.profile
+        alias figlet='figlet -d ~/.fbnmtz/figlet'\n" >> ~/.profile
 fi
